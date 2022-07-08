d54e90054ad0beed2aec55dbee60ba1c60f75c3c
mblazquezcoder <mma@coder.com>
2022-07-08 01:11:18 -0300

    Optimizacion e incorporacion de libreria

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,15 +1,15 @@
 function convertir() {
     var valore = parseInt(document.getElementById("valor").value);
     var resultado = 0;
-    var dolar = 222;
-    var euro = 242;
+    var dolar = 252;
+    var euro = 270;
     if (document.getElementById("uno").checked) {
         resultado = valore / dolar;
         swal("El cambio de pesos a", "Dolares es:$" + resultado.toFixed(2),"success" );
     }
     else if (document.getElementById("dos").checked) {
         resultado = valore / euro;
-        alert("El cambio de pesos a Euros es: "+ resultado.toFixed(2));
+        swal("El cambio de pesos a", "Euros es: "+ resultado.toFixed(2),"success");
     }
     else {
         swal("Completa todos los", "requerimientos","error")
