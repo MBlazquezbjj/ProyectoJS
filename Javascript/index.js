e31295a6cbf5aa5050cc36fa08bbb1b424bfae58
mblazquezcoder <mma@coder.com>
2022-07-07 23:23:21 -0300

    Incorporando libreria

--- Javascript/index.js
+++ Javascript/index.js
@@ -5,14 +5,14 @@ function convertir() {
     var euro = 242;
     if (document.getElementById("uno").checked) {
         resultado = valore / dolar;
-        alert("El cambio de pesos a Dolares es: $" + resultado.toFixed(2));
+        swal("El cambio de pesos a", "Dolares es:$" + resultado.toFixed(2),"success" );
     }
     else if (document.getElementById("dos").checked) {
         resultado = valore / euro;
-        alert("El cambio de pesos a Euros es: " + resultado.toFixed(2));
+        alert("El cambio de pesos a Euros es: "+ resultado.toFixed(2));
     }
     else {
-        alert("Completa todos los requerimientos")
+        swal("Completa todos los", "requerimientos","error")
     }
 }
 
