936acd538eaa2f1c07d67588ec5437d6bac9d0e0
mblazquezcoder <mma@coder.com>
2022-08-01 23:07:59 -0300

    Entrega final

--- Javascript/index.js
+++ Javascript/index.js
@@ -5,14 +5,27 @@ function convertir() {
     var euro = 290;
     if (document.getElementById("uno").checked) {
         resultado = valore / dolar;
-        swal("El cambio de pesos a", "Dolares es:$" + resultado.toFixed(2),"success" );
+        swal("El cambio de pesos a", "Dolares es:$" + resultado.toFixed(2), "success");
     }
     else if (document.getElementById("dos").checked) {
         resultado = valore / euro;
-        swal("El cambio de pesos a", "Euros es: "+ resultado.toFixed(2),"success");
+        swal("El cambio de pesos a", "Euros es: " + resultado.toFixed(2), "success");
     }
     else {
-        swal("Completa todos los", "requerimientos","error")
+        swal("Completa todos los", "requerimientos", "error")
     }
 }
 
+let postsElements = String
+
+fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(Response => {
+        return Response.json()
+    })
+    .then(data => {
+        for (let i = 0; i < data.lenght; i++) {
+            console.log(data)
+            postsElements += "<li>${data[i].userId} - ${data[i].title}</li>"
+        }
+    })
+document.getElementById("posts").innerHTML = postsElements 
--- Javascript/vehiculos.js
+++ Javascript/vehiculos.js
@@ -9,3 +9,40 @@ function contar() {
     }
     document.write("<br> Seleccionaste las marcas: " + n + "favoritas")
 }
+
+const vehiculos = [
+    {
+        "modelo": "peugeot 208",
+        "produccion": 2021,
+        "kilometros": 8000
+    },
+    {
+        "modelo": "ford ka",
+        "produccion": 2022,
+        "kilometros": 1000
+    },
+    {
+        "modelo": "chevrolet cruze",
+        "produccion": 2020,
+        "kilometros": 18000
+    },
+    {
+        "modelo": "volkswagen golf",
+        "produccion": 2022,
+        "kilometros": 0
+    },
+    {
+        "modelo": "renault alaskan",
+        "produccion": 2022,
+        "kilometros": 3000
+    }
+];
+
+JSON.stringify(vehiculos)
+console.log(JSON.stringify(vehiculos))
+
+fetch("./vehiculos.json")
+    .then(Response => Response.json())
+    .then(data => {
+        console.log(data)
+    })
